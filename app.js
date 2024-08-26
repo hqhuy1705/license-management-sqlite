4c04ff23d793f9cd5b46b477993d26cd739b7727
Huy <hqhuy1705@gmail.com>
2024-08-26 23:04:34 +0700

    Add basic authen

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const basicAuth = require('express-basic-auth');
 const licenseRoutes = require('./routes/licenseRoutes');
 const setupDatabase = require('./config/dbSetup'); // Import the database setup function
 const setupSwagger = require('./swagger'); // Import the Swagger setup function
@@ -8,6 +9,17 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// Basic Authentication setup
+const users = {
+    [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD
+  };
+  
+  app.use(basicAuth({
+    users,
+    challenge: true, // This will prompt the browser to show a login dialog
+    unauthorizedResponse: 'Unauthorized'
+  }));
 
 setupSwagger(app); // Set up Swagger
 
